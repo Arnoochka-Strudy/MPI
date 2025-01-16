31480de01f8e689d0f1c50e9f014d2c551e9af83
Arnoochka <kolos.130278@gmail.com>
2025-01-16 09:18:54 +0300

    add host.sh

---
+++ host.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+host=ant.apmath.spbu.ru
+name=NAME
+
+if [ $# -gt 0 ]; then
+    mpiDir=$name@$host:/home/$name
+
+    if [ "$1" == "send" ]; then
+
+        if [ "$2" == "all" ]; then
+            rsync -av --include="?/" --include="??/" --include="Matrix" --include="Vector" --exclude="??*/" --exclude="*" "." "$mpiDir"
+        else
+            scp -r "$2" "$mpiDir"
+        fi
+
+    elif [ "$1" == "recv" ]; then
+
+        if [ "$2" == "all" ]; then
+            rsync -av --include="*/" --include="results*.log" --exclude="*" "$mpiDir" "ssh"
+        else
+
+            mkdir "ssh/$2"
+            scp "$mpiDir/$2/results*.log" "ssh/$2/"
+        fi
+
+    else 
+        echo "uncnown command"
+    fi
+else
+    ssh "$name@$host"
+fi
